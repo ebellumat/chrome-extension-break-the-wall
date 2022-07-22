7e268a4e10b8602bb9cbc5bc975df2a4a03fd22b
Erasmo Bellumat <lbj.erasmo@gmail.com>
2022-07-22 00:52:00 -0300

    Remove jquery, unused permissions and unecessary scripts.

--- content_script.js
+++ content_script.js
@@ -1,42 +1,48 @@
-const createNewTab = () => {
- 
-    chrome.tabs.getSelected(null, function(tab){
-      chrome.storage.sync.set({ "url": tab.url }, function(){
-        window.open('https://outline.com/');
-      });
-  });
-}
+// const createNewTab = () => {
+//   chrome.tabs.getSelected(null, function(tab) {
+//     chrome.storage.sync.set({ url: tab.url }, function() {
+//       window.open(`https://12ft.io/proxy?q=${tab.url}`);
+//     });
+//   });
+// };
 
-const parseContent = () => {
-  chrome.storage.sync.get(/* String or Array */["url"], function(items){
-    if (items && items.url) {
-      $('#source').val(items.url);
-      $('.clean').click();
+// const parseContent = () => {
+//   chrome.storage.sync.get(/* String or Array */ ["url"], function(items) {
+//     if (items && items.url) {
+//       $("#source").val(items.url);
+//       $(".clean").click();
 
-      chrome.storage.sync.set({ "url": null}, function(){
-      });
-    }
-  });
-}
+//       chrome.storage.sync.set({ url: null }, function() {});
+//     }
+//   });
+// };
 
-document.addEventListener('DOMContentLoaded', function() {
-  var link = document.getElementById('target');
-  link.addEventListener('click', function() {
-    createNewTab();
+// document.addEventListener("DOMContentLoaded", function() {
+//   var link = document.getElementById("target");
+//   link.addEventListener("click", function() {
+//     createNewTab();
+//   });
+// });
+
+// document.onreadystatechange = () => {
+//   if (document.readyState === "complete") {
+//     if (location.href.includes("https://outline.com")) {
+//       $("#source").ready(function() {
+//         parseContent();
+//       });
+//     }
+//   }
+// };
+
+chrome.tabs.getSelected(null, function(tab) {
+  chrome.storage.sync.set({ url: tab.url }, function() {
+    window.open(`https://12ft.io/proxy?q=${tab.url}`);
   });
 });
-
-document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
-    if (location.href.includes('https://outline.com')) {
-      $("#source").ready(function(){
-        parseContent();
-     });
-    }
-  }
-};
 
 // accept messages from background
-chrome.runtime.onMessage.addListener (function (request, sender, sendResponse) {
-});
-
+chrome.runtime.onMessage.addListener(function(
+  request,
+  sender,
+  sendResponse
+) {});
